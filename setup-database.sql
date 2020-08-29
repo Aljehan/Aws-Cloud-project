6c2ecebae3583f1dea86c43d5a8d0ac8de8d3d7f
Aljehan <50389780+Aljehan@users.noreply.github.com>
2020-08-29 14:57:37 +1200

    Fixed an error in the second insert statement

--- setup-database.sql
+++ setup-database.sql
@@ -1,4 +1,4 @@
-CREATE TABLE timetable (
+CREATE TABLE timetable(
   code varchar(7),
   day varchar(20) NOT NULL,
   time varchar(10) NOT NULL,
@@ -6,4 +6,4 @@ CREATE TABLE timetable (
 );
 
 INSERT INTO timetable VALUES ('COSC326','Wednesday','10:00 am');
-INSERT INTO timetable VALUES ('COSC349','Tuesday','1:00 pm);
+INSERT INTO timetable VALUES ('COSC349','Tuesday','1:00 pm');
